8e137e88ee66b94776eb3477ea7727f7fb711218
arekkubaczkowski <arek.kubaczkowski@gmail.com>
2021-09-16 12:09:00 +0200

    WIP: app center config (#110)
    * chore: app center ios setup
    
    * chore: metro config
    
    * chore: metro config
    
    * chore: updade podfile.lock
    
    * chore: metro config
    
    * chore: project config revert
    
    * chore: metro config
    
    * chore: app center config
    
    * chore: metro config
    
    * chore: app center android config
    
    * chore: remove flipper
    
    * chore: increase memory
    
    * chore: bump gradle
    
    * chore: .env config for appcenter secrets
    
    * chore: test .env
    
    * chore: pre-build android script
    
    * chore: pre-build android script
    
    * chore: pre-build android script
    
    * chore: pre-build android script
    
    * chore: update plist file
    
    * chore: config scheme
    
    * chore: clean up
    
    * chore: conditional sh script
    
    * chore: add empty appcenter config
    
    * chore: app center sh script imprvement
    
    * chore: upgrade rn slider
    
    * chore: clean up, bump gradle
    
    * chore: improve sh script
    
    * chore: upgrade rn version
    
    * chore: upgrade rn cli
    
    * fix: metro config
    
    * chore: bump library react native version
    
    * chore: revert react native upgrade
    
    * chore: bump rn slider
    
    * chore: temporary remove rn slider
    
    * chore: sh scripit
    
    * chore: bump jetifier
    
    * chore: bump jetifier
    
    * chore: restore slider
    
    * chore: change node_modules directory level
    
    * chore: clean up
    
    * chore: update rn orb
    
    Co-authored-by: Arkadiusz Kubaczkowski <arek.kubaczkowski@callstak.com>

--- example/ios/AmazonIvsExample/AppDelegate.m
+++ example/ios/AmazonIvsExample/AppDelegate.m
@@ -14,21 +14,26 @@
 #import "Orientation.h"
 
 #ifdef FB_SONARKIT_ENABLED
-#import <FlipperKit/FlipperClient.h>
-#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
-#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-static void InitializeFlipper(UIApplication *application) {
-  FlipperClient *client = [FlipperClient sharedClient];
-  SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
-  [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
-  [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
-  [client addPlugin:[FlipperKitReactPlugin new]];
-  [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
-  [client start];
-}
+// AppCenter
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
+
+//#import <FlipperKit/FlipperClient.h>
+//#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
+//#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
+//#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
+//#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
+//#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+//static void InitializeFlipper(UIApplication *application) {
+//  FlipperClient *client = [FlipperClient sharedClient];
+//  SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
+//  [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
+//  [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
+//  [client addPlugin:[FlipperKitReactPlugin new]];
+//  [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
+//  [client start];
+//}
 #endif
 
 @implementation AppDelegate
@@ -36,7 +41,11 @@ static void InitializeFlipper(UIApplication *application) {
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   #ifdef FB_SONARKIT_ENABLED
-    InitializeFlipper(application);
+    [AppCenterReactNative register];
+    [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+    [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
+  
+//    InitializeFlipper(application);
   #endif
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
--- example/metro.config.js
+++ example/metro.config.js
@@ -5,9 +5,12 @@ const pak = require('../package.json');
 
 const root = path.resolve(__dirname, '..');
 
-const modules = Object.keys({
-  ...pak.peerDependencies,
-});
+const modules = [
+  '@babel/runtime',
+  ...Object.keys({
+    ...pak.peerDependencies,
+  }),
+];
 
 module.exports = {
   projectRoot: __dirname,
