47e0e48ffbc3fcd051c32f7c8e130387e9e6283c
Arek Kubaczkowski <arek.kubaczkowski@gmail.com>
2022-06-15 13:21:28 +0200

    fix comments

--- example/android/app/src/main/java/com/example/amazonivsreactnativeplayer/MainApplication.java
+++ example/android/app/src/main/java/com/example/amazonivsreactnativeplayer/MainApplication.java
@@ -28,7 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for StripeTerminalReactNativeExample:
+          // Packages that cannot be autolinked yet can be added manually here, for AmazonIvsExample:
           // packages.add(new MyReactNativePackage());
           packages.add(new AmazonIvsPackage());
           return packages;
