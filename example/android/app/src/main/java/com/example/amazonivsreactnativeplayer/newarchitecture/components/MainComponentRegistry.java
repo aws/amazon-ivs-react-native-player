a865fcfce616c7ea41a8164b9c345622f3aa4662
hurali97 <hurali97@gmail.com>
2022-12-14 21:31:11 +0500

    chore: upgrade android related files

---
+++ example/android/app/src/main/java/com/example/amazonivsreactnativeplayer/newarchitecture/components/MainComponentRegistry.java
@@ -0,0 +1,36 @@
+package com.example.amazonivsreactnativeplayer.newarchitecture.components;
+
+import com.facebook.jni.HybridData;
+import com.facebook.proguard.annotations.DoNotStrip;
+import com.facebook.react.fabric.ComponentFactory;
+import com.facebook.soloader.SoLoader;
+
+/**
+ * Class responsible to load the custom Fabric Components. This class has native methods and needs a
+ * corresponding C++ implementation/header file to work correctly (already placed inside the jni/
+ * folder for you).
+ *
+ * <p>Please note that this class is used ONLY if you opt-in for the New Architecture (see the
+ * `newArchEnabled` property). Is ignored otherwise.
+ */
+@DoNotStrip
+public class MainComponentRegistry {
+  static {
+    SoLoader.loadLibrary("fabricjni");
+  }
+
+  @DoNotStrip private final HybridData mHybridData;
+
+  @DoNotStrip
+  private native HybridData initHybrid(ComponentFactory componentFactory);
+
+  @DoNotStrip
+  private MainComponentRegistry(ComponentFactory componentFactory) {
+    mHybridData = initHybrid(componentFactory);
+  }
+
+  @DoNotStrip
+  public static MainComponentRegistry register(ComponentFactory componentFactory) {
+    return new MainComponentRegistry(componentFactory);
+  }
+}
