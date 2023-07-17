6216f385c4c297f249c672b68353150b4eb32e13
David Whitlark <dawhitla@twitch.tv>
2023-07-17 14:05:42 -0400

    feat: example app now shows current branch and commit hash

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,16 +1,4 @@
-const path = require('path');
-const pak = require('../package.json');
-
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          [pak.name]: path.join(__dirname, '..', `${pak.source}.ts`),
-        },
-      },
-    ],
-  ],
+  plugins: [['module:react-native-dotenv']],
 };
