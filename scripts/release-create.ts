3c9888fd0aa1f80a3497651c7a072064177143e5
David Whitlark <dawhitla@twitch.tv>
2024-01-31 18:19:04 -0500

    feat: enabling rc prerelease versions

--- scripts/release-create.ts
+++ scripts/release-create.ts
@@ -7,42 +7,88 @@ import pkg from '../package.json';
 import { logError, logInfo } from './utils';
 
 async function run() {
+  const skipChecks = !!process.env.SKIP_CHECKS;
   const git = simpleGit();
   const status = await git.status();
 
-  if (status.current !== 'main') {
-    return logError('Must be on main branch to create a release branch');
-  }
+  if (!skipChecks) {
+    if (status.current !== 'main') {
+      return logError('Must be on main branch to create a release branch');
+    }
 
-  if (!status.isClean()) {
-    return logError('Cannot create release with dirty working tree');
-  }
+    if (!status.isClean()) {
+      return logError('Cannot create release with dirty working tree');
+    }
 
-  if (status.ahead > 0) {
-    return logError(
-      `Local commits on main must match with origin/main. You are ${status.ahead} commit(s) ahead`
-    );
-  }
+    if (status.ahead > 0) {
+      return logError(
+        `Local commits on main must match with origin/main. You are ${status.ahead} commit(s) ahead`
+      );
+    }
 
-  if (status.behind > 0) {
-    return logError(
-      `Local commits on main must match with origin/main. You are ${status.behind} commit(s) behind`
-    );
+    if (status.behind > 0) {
+      return logError(
+        `Local commits on main must match with origin/main. You are ${status.behind} commit(s) behind`
+      );
+    }
   }
 
   try {
-    const versions = (['major', 'minor', 'patch'] as semver.ReleaseType[]).map(
-      (type) => {
-        const version = semver.inc(pkg.version, type);
-        return { name: `${type} ${version}`, value: version };
+    // different behavior based on rc version or not
+    const prerelease = (semver.prerelease(pkg.version) ?? []).length;
+
+    let bumps = prerelease
+      ? ['prereleasenext', 'prereleasepatch', 'prereleasedone']
+      : ['premajor', 'preminor', 'prepatch', 'major', 'minor', 'patch'];
+
+    const versions = bumps.map((type) => {
+      let version: string | null = pkg.version;
+      if (version) {
+        const prereleasepatch = prerelease > 2;
+        switch (type) {
+          case 'prereleasenext':
+            version = semver.inc(
+              prereleasepatch ? version.replace(/\.[^.]*$/gm, '') : version,
+              'prerelease',
+              'rc',
+              '1'
+            );
+            break;
+          case 'prereleasepatch':
+            version = prereleasepatch
+              ? semver.inc(version, 'prerelease', 'rc', '1')
+              : `${version}.2`;
+            break;
+          case 'prereleasedone':
+            version = version.replace(/-rc.*/gm, '');
+            break;
+          case 'premajor':
+          case 'preminor':
+          case 'prepatch':
+          case 'prerelease':
+            version = semver.inc(
+              version,
+              type as semver.ReleaseType,
+              'rc',
+              '1'
+            );
+            break;
+          default:
+            version = semver.inc(version, type as semver.ReleaseType);
+            break;
+        }
+        if (!version) {
+          throw new Error(`Invalid version: ${version}`);
+        }
       }
-    );
+      return { name: `${type} ${version}`, value: version };
+    });
 
     const answers = await inquirer.prompt([
       {
         type: 'list',
         name: 'bump',
-        message: 'Select release type',
+        message: `${pkg.version}: select release type`,
         default: versions[1].value,
         choices: versions,
       },
