3b3fae6d6c86bee152fea27fbc4f4d0001cf3d27
Juozas Petkelis <juozas.petkelis@callstack.com>
2025-11-03 17:09:53 +0200

    chore: update to RN 0.81.5

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
 const escape = require('escape-string-regexp');
 const pak = require('../package.json');
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 const root = path.resolve(__dirname, '..');
+
+const defaultConfig = getDefaultConfig(__dirname);
+const {
+  resolver: { blockList: defaultBlockList },
+} = defaultConfig;
 
 const modules = [
   '@babel/runtime',
@@ -13,20 +17,22 @@ const modules = [
   }),
 ];
 
+const modulesToBlock = modules.map(
+  (m) =>
+    new RegExp(
+      // Use OS-agnostic path separator ([\/\\]) as recommended
+      `^${escape(path.join(root, 'node_modules', m))}[\\/\\\\].*$`
+    )
+);
+
 const config = {
   projectRoot: __dirname,
   watchFolders: [root],
 
   // We need to make sure that only one version is loaded for peerDependencies
-  // So we blacklist them at the root, and alias them to the versions in example's node_modules
+  // So we blockList them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    blacklistRE: exclusionList(
-      modules.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-      )
-    ),
-
+    blockList: modulesToBlock.concat(defaultBlockList || []),
     extraNodeModules: modules.reduce(
       (acc, name) => {
         acc[name] = path.join(__dirname, 'node_modules', name);
