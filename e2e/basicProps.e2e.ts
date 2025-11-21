511810e29f33757f048ffd13f126e1e5a007815f
Juozas Petkelis <juozas.petkelis@callstack.com>
2025-11-21 14:12:49 +0200

    feat: add yarn hoisting config

--- e2e/basicProps.e2e.ts
+++ e2e/basicProps.e2e.ts
@@ -2,39 +2,17 @@
 import {
   afterAllTestPlan,
   beforeAllTestPlan,
+  tempHold,
   waitForClearLogs,
-  waitForTestPlan,
+  waitForLogID,
   waitForLogLabel,
   waitForTap,
-  waitForLogID,
+  waitForTestPlan,
 } from './testPlan';
 
 describe('Basic Props', () => {
   beforeAll(beforeAllTestPlan);
   afterAll(afterAllTestPlan);
-
-  it('paused controls playback state', async () => {
-    await waitForTestPlan(`
-    inputs:
-    - paused: true
-    events:
-    - onPlayerStateChange
-    `);
-    await waitForTap(by.id('paused'));
-    await waitForLogLabel('onPlayerStateChange ::: Playing');
-  });
-
-  it('paused controls playback state', async () => {
-    await waitForTestPlan(`
-    inputs:
-    - paused: false
-    events:
-    - onPlayerStateChange
-    `);
-    await waitForLogLabel('onPlayerStateChange ::: Playing');
-    await waitForTap(by.id('paused'));
-    await waitForLogLabel('onPlayerStateChange ::: Idle');
-  });
 
   it('autoMaxQuality controls highest quality picked on auto', async () => {
     await waitForTestPlan(`
@@ -44,9 +22,34 @@ describe('Basic Props', () => {
     - onQualityChange
     `);
     await waitForLogID('onQualityChange', 30);
+    await tempHold(4);
+    await element(by.id('testPlan')).tapReturnKey();
     await waitForClearLogs();
-    // bump down - increase timeout for this flaky element
-    await waitForTap(by.id('autoMaxQuality:160p'), 40);
+
+    await waitForTap(by.id('autoMaxQuality:160p'), 4);
     await waitForLogLabel('onQualityChange ::: name ::: 160p', 30);
+  });
+
+  it('paused controls playback state', async () => {
+    await waitForTestPlan(`
+      inputs:
+      - paused: true
+      events:
+      - onPlayerStateChange
+      `);
+    await waitForTap(by.id('paused'));
+    await waitForLogLabel('onPlayerStateChange ::: Playing');
+  });
+
+  it('paused controls playback state', async () => {
+    await waitForTestPlan(`
+      inputs:
+      - paused: false
+      events:
+      - onPlayerStateChange
+      `);
+    await waitForLogLabel('onPlayerStateChange ::: Playing');
+    await waitForTap(by.id('paused'));
+    await waitForLogLabel('onPlayerStateChange ::: Idle');
   });
 });
